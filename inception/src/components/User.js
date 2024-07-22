cdb620d40ad6e6e22ac4e19b76ab7beebd9adbed
Nitesh Bagle <niteshbagle43@gmail.com>
2024-07-22 11:42:34 +0530

    state in class based component

--- inception/src/components/User.js
+++ inception/src/components/User.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const User = (props) => {
+  const [count, setCount] = useState(0);
   const { name, location } = props;
   return (
     <div className='user-card'>
+        <p>Counter: {count}</p>
         <h2>{name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: test</h4>
--- inception/src/components/UserClass.js
+++ inception/src/components/UserClass.js
@@ -3,12 +3,18 @@ import React from "react";
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0
+        }
     }
 
     render() {
         const { name, location } = this.props;
+        const { count } = this.state;
         return (
             <div className='user-card'>
+                <p>Counter: {count}</p>
+                <button onClick={() => this.setState({ ...this.state, count: this.state.count + 1 })}>Counter</button>
                 <h2>{name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: test</h4>
