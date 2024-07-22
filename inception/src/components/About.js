36161e518136e047cd0d40ac5398f39371c655a6
Nitesh Bagle <niteshbagle43@gmail.com>
2024-07-22 12:49:08 +0530

    class component life cycle

--- inception/src/components/About.js
+++ inception/src/components/About.js
@@ -1,15 +1,30 @@
+import React from "react";
 import User from "./User";
 import UserClass from './UserClass';
 
-const About = () => {
-    return (
-        <div className="about">
-            <h1>About</h1>
-            <h2>This is about page details</h2>
-            <User name="Nitesh Bagle" location="Ulhasnagar"/>
-            <UserClass name="Nitesh Bagle" location="Ulhasnagar"/>
-        </div>
-    )
-};
+class About extends React.Component {
+    constructor (props) {
+        super(props);
+        console.log('About constructor');
+    }
+
+    componentDidMount() {
+        console.log('About componentDidMount')
+    }
+
+    render () {
+        console.log('About render');
+        return (
+            <div className="about">
+                <h1>About</h1>
+                <h2>This is about page details</h2>
+                <User name="Nitesh Bagle" location="Ulhasnagar"/>
+                <UserClass name="Nitesh Bagle" location="Ulhasnagar" user="user1"/>
+                <UserClass name="Nitesh Bagle" location="Ulhasnagar" user="user2"/>
+                <UserClass name="Nitesh Bagle" location="Ulhasnagar" user="user3"/>
+            </div>
+        );
+    }
+}
 
 export default About;
--- inception/src/components/UserClass.js
+++ inception/src/components/UserClass.js
@@ -6,15 +6,22 @@ class UserClass extends React.Component {
         this.state = {
             count: 0
         }
+        console.log('UserClass constructor');
+    }
+
+    componentDidMount() {
+        console.log('UserClass componentDidMount', this.props.user)
+        // api call
     }
 
     render() {
+        console.log('UserClass render', this.props.user);
         const { name, location } = this.props;
         const { count } = this.state;
         return (
             <div className='user-card'>
                 <p>Counter: {count}</p>
-                <button onClick={() => this.setState({ ...this.state, count: this.state.count + 1 })}>Counter</button>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>Counter</button>
                 <h2>{name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: test</h4>
